534a2d12eaeee95a231dcda3e911819b08191cb0
parkseongbeen <parksbeen03@snu.ac.kr>
2023-09-12 17:46:00 +0900

    background

--- app/src/main/java/com/example/setting/SettingActivity.java
+++ app/src/main/java/com/example/setting/SettingActivity.java
@@ -2,13 +2,19 @@ package com.example.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class SettingActivity extends AppCompatActivity {
     TextView textView;
+    Button button_red, button_blue, button_green;
+    LinearLayout layout;
     AudioManager audioManager=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,15 @@ public class SettingActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.volume);
         SeekBar seekBar = findViewById(R.id.seekBar);
+
+        button_red = findViewById(R.id.button_red);
+        button_blue = findViewById(R.id.button_blue);
+        button_green = findViewById(R.id.button_green);
+        layout = findViewById(R.id.setting);
+
+        button_red.setOnClickListener(ColorClick);
+        button_blue.setOnClickListener(ColorClick);
+        button_green.setOnClickListener(ColorClick);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -34,5 +49,18 @@ public class SettingActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+        View.OnClickListener ColorClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = view.getId();
+                if (id == R.id.button_red) {
+                    layout.setBackgroundColor(Color.rgb(255, 199, 234));
+                } else if (id == R.id.button_blue) {
+                    layout.setBackgroundColor(Color.rgb(130, 160, 216));
+                } else if (id == R.id.button_green) {
+                    layout.setBackgroundColor(Color.rgb(166, 255, 150));
+                }
+            }
+        };
     }
 }
